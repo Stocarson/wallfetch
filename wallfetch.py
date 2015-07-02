c330652c021fc33c9078115cd80a6b0a995f7894
Niccolò Izzo <izzo.niccolo@gmail.com>
2015-07-02 20:53:26 +0200

    Now code is conform to pep-8 (thanks edne).

--- wallfetch.py
+++ wallfetch.py
@@ -4,37 +4,42 @@ import re
 import urllib
 import multiprocessing as mp
 import argparse
-from multiprocessing import Process, Pool
-
+from multiprocessing import Pool
+ 
+ 
 def downloadAll():
-    parameters = {"categories":"111", "purity":"110", "sorting":"random", "order":"desc"}
+    parameters = {"categories": "111", "purity": "110",
+                  "sorting": "random", "order": "desc"}
     parser = argparse.ArgumentParser()
-    parser.add_argument("keyword", nargs = "?")
-    parser.add_argument("-s", "--sfw", action="store_true", help = "show only safe for work wallpapers")
+    parser.add_argument("keyword", nargs="?")
+    parser.add_argument("-s", "--sfw", action="store_true",
+                        help="show only safe for work wallpapers")
     args = parser.parse_args()
     if args.sfw:
         parameters["purity"] = "100"
     if args.keyword is not None:
         parameters["q"] = args.keyword
     sourceURL = "http://alpha.wallhaven.cc/search"
-    response = requests.get(sourceURL, params = parameters)
+    response = requests.get(sourceURL, params=parameters)
     html = response.content.decode("utf-8")
     images = re.findall('data-wallpaper.id="(.*?)"', html)
-    pool = Pool(processes = mp.cpu_count())
+    pool = Pool(processes=mp.cpu_count())
     pool.map(downloadImage, images)
-            
+ 
+ 
 def downloadImage(index):
     imageURL = "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"
-    print("Downloading image no "+index)
+    print("Downloading image no " + index)
     try:
-        r = urllib.request.urlopen(imageURL+index+".jpg")
-        f = open(index+".jpg", "bw")
+        r = urllib.request.urlopen(imageURL + index + ".jpg")
+        f = open(index + ".jpg", "bw")
         f.write(r.read())
     except urllib.error.URLError:
-        print("Cannot download image number "+index)
-
+        print("Cannot download image number " + index)
+ 
+ 
 def main():
     downloadAll()
-
+ 
 if __name__ == "__main__":
     main()
