a67477bd11086c85a24df9539f285b91bd0ec35d
Niccolò Izzo <izzo.niccolo@gmail.com>
2015-07-29 16:46:21 +0200

    Added fake User-Agent header.

--- wallfetch.py
+++ wallfetch.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import requests
 import re
-import urllib
+import urllib.request
 import multiprocessing as mp
 import argparse
 from multiprocessing import Pool
- 
- 
+
 def downloadAll():
     parameters = {"categories": "111", "purity": "110",
                   "sorting": "random", "order": "desc"}
@@ -26,17 +25,18 @@ def downloadAll():
     pool = Pool(processes=mp.cpu_count())
     pool.map(downloadImage, images)
  
- 
 def downloadImage(index):
     imageURL = "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"
     print("Downloading image no " + index)
     try:
-        r = urllib.request.urlopen(imageURL + index + ".jpg")
+        url = imageURL + index + ".jpg"
+        userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " \
+                "(KHTML, like Gecko) Ubuntu Chromium/43.0.2357.130 Chrome/43.0.2357.130 Safari/537.36"
+        r = urllib.request.urlopen(urllib.request.Request(url, headers={"User-Agent": userAgent}))
         f = open(index + ".jpg", "bw")
         f.write(r.read())
     except urllib.error.URLError:
         print("Cannot download image number " + index)
- 
  
 def main():
     downloadAll()
